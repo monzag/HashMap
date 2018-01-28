d49a1f114190caa61fa9e21cfe01f00985f45dda
monzag <zaganiacz.m@gmail.com>
2018-01-28 23:27:50 +0100

    Add add method

--- src/main/java/com/monzag/HashMap.java
+++ src/main/java/com/monzag/HashMap.java
@@ -6,6 +6,19 @@ public class HashMap {
 
     private int size = 16;
     private LinkedList<KeyValue>[] elements = new LinkedList[size];
-    
+
+    public void add(String key, Integer value) {
+        int position = getHash(key);
+        LinkedList<KeyValue> list = elements[position];
+
+        for (KeyValue pair : list) {
+            if (pair.key == key) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        list.add(new KeyValue(key,value));
+        resizeIfNeeded();
+    }
 
 }
