bc8c71a16545d90bfb2af6669d7bdac1a9d22590
monzag <zaganiacz.m@gmail.com>
2018-01-28 23:23:23 +0100

    Add field: elements

--- src/main/java/com/monzag/HashMap.java
+++ src/main/java/com/monzag/HashMap.java
@@ -1,8 +1,11 @@
 package com.monzag;
 
+import java.util.LinkedList;
+
 public class HashMap {
 
     private int size = 16;
+    private LinkedList<KeyValue>[] elements = new LinkedList[size];
     
 
 }
