f5ff8f8a842a08660899c31e2e64b379597a6dd4
monzag <zaganiacz.m@gmail.com>
2018-01-29 00:54:23 +0100

    Add App class

---
+++ src/main/java/com/monzag/App.java
@@ -0,0 +1,14 @@
+package com.monzag;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        HashMap map = new HashMap();
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+        System.out.println("Map: " + map.toString());
+        
+    }
+}
