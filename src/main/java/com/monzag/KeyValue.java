e26831dcc89e92102176b4b7f1eb3bbd57cac221
monzag <zaganiacz.m@gmail.com>
2018-01-28 23:21:16 +0100

    Remove next field

--- src/main/java/com/monzag/KeyValue.java
+++ src/main/java/com/monzag/KeyValue.java
@@ -4,11 +4,9 @@ public class KeyValue {
 
     public String key;
     public Integer value;
-    public KeyValue next;
 
     public KeyValue(String key, Integer value) {
         this.key = key;
         this.value = value;
-        this.next = null;
     }
 }
